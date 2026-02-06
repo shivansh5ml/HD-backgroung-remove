4d63c84ccc834a3aa89875f7ad82f9c092ad18a7
shivansh5ml <freefiresumant@gmail.com>
2026-02-06 11:19:27 +0530

    Create script.js
    Js

---
+++ script.js
@@ -0,0 +1,21 @@
+async function removeBg() {
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+
+  const formData = new FormData();
+  formData.append("image_file", file);
+  formData.append("size", "auto");
+
+  const response = await fetch("https://api.remove.bg/v1.0/removebg", {
+    method: "POST",
+    headers: {
+      "X-Api-Key": "JruE55A9uf3xwgeWNUc74RZh"
+    },
+    body: formData
+  });
+
+  const blob = await response.blob();
+  const imgURL = URL.createObjectURL(blob);
+  document.getElementById("resultImg").src = imgURL;
+  document.getElementById("downloadBtn").href = imgURL;
+}
